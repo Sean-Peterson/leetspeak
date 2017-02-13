6859abb5aeb5dacafaeb622bc4f03c0af6668f1a
Philip Putnam and Sean Peterson <sean@>
2017-02-13 14:04:44 -0800

    add stoz and notFirstS functionality

--- src/LeetspeakTranslator.php
+++ src/LeetspeakTranslator.php
@@ -5,13 +5,16 @@
         {
             $exploded = str_split($input);
             $newWord = array();
-            foreach ($exploded as $character){
+            foreach ($exploded as $key => $character){
                 $push_letter = $character;
                 if ($character == 'e' || $character == 'E'){
-                     $push_letter = '3';
-                }
-                if ($character == 'o' || $character == 'O') {
+                    $push_letter = '3';
+                } elseif ($character == 'o' || $character == 'O') {
                     $push_letter = '0';
+                } elseif ($character == 'I') {
+                    $push_letter = '1';
+                } elseif (($character == 's' || $character == 'S') && $exploded[$key - 1] != " ") {
+                    $push_letter = 'z';
                 }
                 array_push($newWord, $push_letter);
             }
--- tests/LeetspeakTranslatorTest.php
+++ tests/LeetspeakTranslatorTest.php
@@ -28,5 +28,58 @@
             //assert
             $this->assertEquals("d03", $result);
         }
+
+        function test_multipleWords()
+        {
+            //arrange
+            $test_LeetspeakTranslator = new LeetspeakTranslator;
+            $input = "the doe";
+
+            //act
+            $result = $test_LeetspeakTranslator->translator($input);
+
+            //assert
+            $this->assertEquals("th3 d03", $result);
+        }
+
+        function test_switchIToOne()
+        {
+            //arrange
+            $test_LeetspeakTranslator = new LeetspeakTranslator;
+            $input = "Ian the doe";
+
+            //act
+            $result = $test_LeetspeakTranslator->translator($input);
+
+            //assert
+            $this->assertEquals("1an th3 d03", $result);
+        }
+
+        function test_switchSToZ()
+        {
+            //arrange
+            $test_LeetspeakTranslator = new LeetspeakTranslator;
+            $input = "Inside the loop";
+
+            //act
+            $result = $test_LeetspeakTranslator->translator($input);
+
+            //assert
+            $this->assertEquals("1nzid3 th3 l00p", $result);
+        }
+
+        function test_switchNotFirstS()
+        {
+            //arrange
+            $test_LeetspeakTranslator = new LeetspeakTranslator;
+            $input = "Sean slams IPA's like an ogre";
+
+            //act
+            $result = $test_LeetspeakTranslator->translator($input);
+
+            //assert
+            $this->assertEquals("S3an slamz 1PA'z lik3 an 0gr3", $result);
+        }
+
     }
 ?>
