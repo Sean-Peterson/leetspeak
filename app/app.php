97dca915986ee0e88fc30f5a6838127aa5171f9c
Philip Putnam and Sean Peterson <philip@>
2017-02-13 16:18:19 -0800

    add user-interface

--- app/app.php
+++ app/app.php
@@ -10,7 +10,12 @@
     $app->get('/', function() use ($app) {
 
         return $app['twig']->render('index.html.twig');
+    });
 
+    $app->post('/leet', function() use ($app) {
+        $new_leet = new LeetspeakTranslator;
+        $new_leetspeek = $new_leet->translator($_POST['word']);
+        return $app['twig']->render('leet.html.twig', array('phrases' => $new_leetspeek));
     });
 
     return $app;
